5e62c258b866cc0771ae918a94b79ef628f01d45
cevdetoz <cevdetoz047@gmail.com>
2020-11-18 15:18:53 +0200

    Ikinci commit

--- src/BirinciGun.java
+++ src/BirinciGun.java
@@ -1,6 +1,7 @@
 
 public class BirinciGun {
 public static void main(String[] args) {
-	System.out.println("Birinci gun");
+	System.out.println("11:57-Birinci gun");
+	System.out.println("Ikinci commit");
 }
 }
