b8b4f0476337fbaeb10bf2b53ea885472fe64eac
cevdetoz <74539562+cevdetoz@users.noreply.github.com>
2020-11-18 16:17:01 +0200

    Update BirinciGun.java

--- src/BirinciGun.java
+++ src/BirinciGun.java
@@ -3,5 +3,7 @@ public class BirinciGun {
 public static void main(String[] args) {
 	System.out.println("11:57-Birinci gun");
 	System.out.println("Ikinci commit");
+	System.out.println("10:11 -Ikinci gun");
+		
 }
 }
