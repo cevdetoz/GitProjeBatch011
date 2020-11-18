8e0675a146374698cd5f782ff5f2e1c8f1be0cb9
cevdetoz <74539562+cevdetoz@users.noreply.github.com>
2020-11-18 16:53:20 +0200

    Update BirinciGun.java

--- src/BirinciGun.java
+++ src/BirinciGun.java
@@ -7,6 +7,7 @@ public static void main(String[] args) {
 	System.out.println("10:26 -ikinci gun");
 	System.out.println("10:35 -ikinci gun");
 System.out.println("11:05 -local");
+System.out.println("11:23 -github.com");
 	
 
 }
